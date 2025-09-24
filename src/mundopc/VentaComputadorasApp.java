8d863c094f0ca59e34a282882a7aa64c5bec1d08
AnaJotta <anajessicamarinmorales@gmail.com>
2025-09-24 23:44:16 +0200

    subida

---
+++ src/mundopc/VentaComputadorasApp.java
@@ -0,0 +1,21 @@
+package mundopc;
+
+import mundopc.modelo.Computadora;
+import mundopc.modelo.Monitor;
+import mundopc.modelo.Raton;
+import mundopc.modelo.Teclado;
+
+public class Main {
+    public static void main(String[] args) {
+    //Crear objetos
+        Raton ratonLenovo = new Raton("bluetooth", "Lenovo");
+        System.out.println(ratonLenovo);
+        Teclado tecladoLenovo = new Teclado("bluetooth", "Lenovo");
+        System.out.println(tecladoLenovo);
+        Monitor monitorLenovo= new Monitor("Lenovo", 45);
+        System.out.println(monitorLenovo);
+        Computadora computadoraLenovo=new Computadora("Lenovo", monitorLenovo, tecladoLenovo, ratonLenovo);
+        System.out.println(computadoraLenovo);
+
+    }
+}
